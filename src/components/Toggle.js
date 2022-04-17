d4f2bb6e46aedf727358c2d50afb58d277f6497f
yumin <dorayuminlee@gmail.com>
2022-04-17 15:17:01 +0900

    [feat] Toggle 부가요소 (슬라이드) 추가

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
 
 export default function Toggle() {
+  // toggled == 상세
   const [toggled, setToggled] = useState(false);
+  const onClickDefault = () => {
+    setToggled(false);
+  }
+  const onClickDetails = () => {
+    setToggled(true);
+  }
   return (
     <>
       <h2 className="subtitle">1. Toggle</h2>
+      <label className="toggle-switch">
+        <input type="checkbox" checked={toggled} />
+        <span className="toggle-slider" />
+      </label>
       <div className="toggle-wrap">
-        <div className="toggle-slider">
-          <div
-            className={toggled ? "toggle-default" : "toggle-selected"}
-            onClick={() => setToggled(false)}
-          >
-            기본
-          </div>
-          <div
-            className={toggled ? "toggle-selected" : "toggle-default"}
-            onClick={() => setToggled(true)}
-          >
-            상세
-          </div>
+        <div className={toggled ? "default" : "dark"} onClick={onClickDefault}>
+          기본
+        </div>
+        <div className={toggled ? "dark" : "default"}  onClick={onClickDetails}>
+          상세
         </div>
       </div>
       <p>선택된 항목은 " {toggled ? "상세" : "기본"} "</p>
