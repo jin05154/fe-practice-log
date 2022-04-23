b5cbfaa142b114aa00b1a552c943b78960397339
yumin <dorayuminlee@gmail.com>
2022-04-23 19:36:14 +0900

    [feat] Slider 부가요소 구현

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 export default function Slider() {
   const [rangeVal, setRangeVal] = useState(1);
+  const handleMilestone = (num) => setRangeVal(num);
 
   return (
     <>
@@ -11,7 +13,7 @@ export default function Slider() {
         <span>%</span>
       </div>
       <label className="slider-wrap">
-        <input
+        <StyledSlider
           type="range"
           min={1}
           max={100}
@@ -20,14 +22,29 @@ export default function Slider() {
           className="slider"
           readOnly
         />
+        <div className="milestones">
+          <div id="colored" />
+          <div id={rangeVal >= 25 ? "colored" : null} />
+          <div id={rangeVal >= 50 ? "colored" : null} />
+          <div id={rangeVal >= 75 ? "colored" : null} />
+          <div id={rangeVal === 100 ? "colored" : null} />
+        </div>
         <div className="milestone-nums">
-          <div>1%</div>
-          <div>25%</div>
-          <div>50%</div>
-          <div>75%</div>
-          <div>100%</div>
+          <div onClick={() => handleMilestone(1)}>1%</div>
+          <div onClick={() => handleMilestone(25)}>25%</div>
+          <div onClick={() => handleMilestone(50)}>50%</div>
+          <div onClick={() => handleMilestone(75)}>75%</div>
+          <div onClick={() => handleMilestone(100)}>100%</div>
         </div>
       </label>
     </>
   );
 }
+
+const StyledSlider = styled.input`
+  background: linear-gradient(
+    to right,
+    var(--mint-green) 0% ${(props) => props.value}%,
+    var(--border-default) ${(props) => props.value}% 100%
+  );
+`;
